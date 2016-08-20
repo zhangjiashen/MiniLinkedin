1b0b3dd62a1d6115df6844ff56de1f20cd308263
guojing <dsofterwolf@gmail.com>
2016-08-20 16:11:58 -0700

    Deleted unnecessary files

--- app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
@@ -1,12 +1,7 @@
 package com.jiuzhang.guojing.awesomeresume;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.jiuzhang.guojing.awesomeresume.model.BasicInfo;
@@ -34,57 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setupUI();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void setupUI() {
         setContentView(R.layout.activity_main);
-
-        ImageButton addEducationBtn = (ImageButton) findViewById(R.id.add_education_btn);
-        addEducationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageButton addExperienceBtn = (ImageButton) findViewById(R.id.add_experience_btn);
-        addExperienceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExperienceEditActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageButton addProjectBtn = (ImageButton) findViewById(R.id.add_project_btn);
-        addProjectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProjectEditActivity.class);
-                startActivity(intent);
-            }
-        });
 
         setupBasicInfo();
         setupEducation();
