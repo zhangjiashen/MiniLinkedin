aa4ab05379a7d6ea99069c8e83e757b7610d5a54
Bonan Dong <bd343@cornell.edu>
2016-10-30 23:14:03 -0700

    Create TODO

--- app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
@@ -1,21 +1,11 @@
 package com.jiuzhang.guojing.awesomeresume;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jiuzhang.guojing.awesomeresume.model.BasicInfo;
 import com.jiuzhang.guojing.awesomeresume.model.Education;
-import com.jiuzhang.guojing.awesomeresume.model.Experience;
-import com.jiuzhang.guojing.awesomeresume.model.Project;
 import com.jiuzhang.guojing.awesomeresume.util.DateUtils;
 
 import java.util.ArrayList;
@@ -24,12 +14,8 @@ import java.util.List;
 @SuppressWarnings("ConstantConditions")
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQ_CODE_EDUCATION_EDIT = 100;
-
     private BasicInfo basicInfo;
-    private List<Education> educations;
-    private Experience experience;
-    private Project project;
+    private Education education;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,140 +25,22 @@ public class MainActivity extends AppCompatActivity {
         setupUI();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQ_CODE_EDUCATION_EDIT && resultCode == Activity.RESULT_OK) {
-            Education education = data.getParcelableExtra(EducationEditActivity.KEY_EDUCATION);
-            updateEducation(education);
-        }
-    }
-
-    private void updateEducation(Education education) {
-        boolean found = false;
-        for (int i = 0; i < educations.size(); ++i) {
-            Education e = educations.get(i);
-            if (TextUtils.equals(education.id, e.id)) {
-                educations.set(i, education);
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            educations.add(education);
-        }
-
-        setupEducations();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void setupUI() {
         setContentView(R.layout.activity_main);
 
-        ImageButton addEducationBtn = (ImageButton) findViewById(R.id.add_education_btn);
-        addEducationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
-                startActivityForResult(intent, REQ_CODE_EDUCATION_EDIT);
-            }
-        });
-
-        ImageButton addExperienceBtn = (ImageButton) findViewById(R.id.add_experience_btn);
-        addExperienceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExperienceEditActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ImageButton addProjectBtn = (ImageButton) findViewById(R.id.add_project_btn);
-        addProjectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProjectEditActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        setupBasicInfo();
-        setupEducations();
-        setupExperience();
-        setupProject();
+        setupBasicInfoUI();
+        setupEducationUI();
     }
 
-    private void setupBasicInfo() {
+    private void setupBasicInfoUI() {
         ((TextView) findViewById(R.id.name)).setText(basicInfo.name);
         ((TextView) findViewById(R.id.email)).setText(basicInfo.email);
     }
 
-    private void setupEducations() {
-        LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.educations);
-        educationsLayout.removeAllViews();
-        for (Education education : educations) {
-            educationsLayout.addView(getEducationView(education));
-        }
-    }
-
-    private View getEducationView(final Education education) {
-        View view = getLayoutInflater().inflate(R.layout.education_item, null);
-        String dateString = DateUtils.dateToString(education.startDate)
-                + " ~ " + DateUtils.dateToString(education.endDate);
-        ((TextView) view.findViewById(R.id.education_school))
-                .setText(education.school + " (" + dateString + ")");
-        ((TextView) view.findViewById(R.id.education_courses))
-                .setText(formatItems(education.courses));
-        view.findViewById(R.id.edit_education_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
-                intent.putExtra(EducationEditActivity.KEY_EDUCATION, education);
-                startActivityForResult(intent, REQ_CODE_EDUCATION_EDIT);
-            }
-        });
-        return view;
-    }
-
-    private void setupExperience() {
-        String dateString = DateUtils.dateToString(experience.startDate)
-                + " ~ " + DateUtils.dateToString(experience.endDate);
-        ((TextView) findViewById(R.id.experience_company))
-                .setText(experience.company + " (" + dateString + ")");
-        ((TextView) findViewById(R.id.experience_details))
-                .setText(formatItems(experience.details));
-    }
-
-    private void setupProject() {
-        String dateString = DateUtils.dateToString(project.startDate)
-                + " ~ " + DateUtils.dateToString(project.endDate);
-        ((TextView) findViewById(R.id.project_name))
-                .setText(project.name + " (" + dateString + ")");
-        ((TextView) findViewById(R.id.project_details))
-                .setText(formatItems(project.details));
+    private void setupEducationUI() {
+        // TODO 3: Display the education data onto the UI
+        // Follow the example in setupBasicInfoUI
+        // You will probably find formatItems method useful when displaying the courses
     }
 
     private void fakeData() {
@@ -180,54 +48,17 @@ public class MainActivity extends AppCompatActivity {
         basicInfo.name = "Jing Guo";
         basicInfo.email = "guojing@jiuzhang.com";
 
-        Education education1 = new Education();
-        education1.school = "THU";
-        education1.major = "Computer Science";
-        education1.startDate = DateUtils.stringToDate("09/2013");
-        education1.endDate = DateUtils.stringToDate("09/2015");
+        education = new Education();
+        education.school = "THU";
+        education.major = "Computer Science";
+        education.startDate = DateUtils.stringToDate("09/2013");
 
-        education1.courses = new ArrayList<>();
-        education1.courses.add("Database");
-        education1.courses.add("Algorithm");
-        education1.courses.add("OO Design");
-        education1.courses.add("Operating System");
+        // TODO 1: Set the endDate
+        // Follow the above example for startDate
+        // DateUtils is a class written by ourselves, check out util/DateUtils
 
-        Education education2 = new Education();
-        education2.school = "CMU";
-        education2.major = "Computer Science";
-        education2.startDate = DateUtils.stringToDate("09/2015");
-        education2.endDate = DateUtils.stringToDate("09/2018");
-
-        education2.courses = new ArrayList<>();
-        education2.courses.add("course 1");
-        education2.courses.add("course 2");
-
-        educations = new ArrayList<>();
-        educations.add(education1);
-        educations.add(education2);
-
-        experience = new Experience();
-        experience.company = "LinkedIn";
-        experience.title = "Software Engineer";
-        experience.startDate = DateUtils.stringToDate("09/2015");
-        experience.endDate = DateUtils.stringToDate("04/2016");
-
-        List<String> experienceDetails = new ArrayList<>();
-        experienceDetails.add("Built something using some tech");
-        experienceDetails.add("Built something using some tech");
-        experienceDetails.add("Built something using some tech");
-        experience.details = experienceDetails;
-
-        project = new Project();
-        project.name = "AwesomeResume - an Android resume app";
-        project.startDate = DateUtils.stringToDate("10/2015");
-        project.endDate = DateUtils.stringToDate("11/2015");
-
-        List<String> projectDetails = new ArrayList<>();
-        projectDetails.add("Completed xxx using xxx");
-        projectDetails.add("Completed xxx using xxx");
-        projectDetails.add("Completed xxx using xxx");
-        project.details = projectDetails;
+        // TODO 2: Add some fake courses in education1.courses
+        education.courses = new ArrayList<>();
     }
 
     public static String formatItems(List<String> items) {
---
+++ readme.txt
@@ -0,0 +1,27 @@
+Please check out the following branches in order
+1. 1_basic_info_ui: basic info part of the main UI
+2. 2_main_ui: all the main UI
+3. 3_fake_data: make the UI display fake data
+4. 4_master: the complete version
+
+Current branch: 3_fake_data
+
+Goals:
+In this branch, instead of hard coding the content of the UI in activity_main.xml, we fake data
+in MainActivity.java and use Java code to display the fake data onto the UI.
+
+Remember what making UI is about? It's all about displaying and manipulating the data! You have just
+learned how to display it! Congratulations!
+
+Workflow:
+Check out the todos and follow the instructions.
+Check out "preview.png" to see what it should look like.
+
+Challenge:
+Finish Experience and Project part (use Education as a reference)
+
+Advise:
+If you have some questions, it is better to search in google(baidu) before asking someone else. You need PRACTICE to learn how to get what you what.
+If you can not find answer, feel free to post your question in jiuzhang.com/qa or QQ group.
+You can see complete code(including code of Challenge) in "master" branch.
+It is always the last step to compare your code with "master"
